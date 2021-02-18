9cfaaa6876c2b139354bf3d28721598bca2e059b
kyawkhaung <kyawkhaung@gmail.com>
2021-02-18 14:05:34 +0800

    Update app.py

--- app.py
+++ app.py
@@ -143,7 +143,5 @@ def detail(articleid):
 
 
 if __name__=="__main__":
-    #app.run(debug=True)
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
-
+    app.run(debug=True)
+    
